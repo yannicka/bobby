fd3fcbd89e48e032e6fea0d1e91ad7d359f58d69
Yannick Armand <yannick.armand@protonmail.com>
2019-08-24 21:15:42 +0200

    Première interface de sélection de niveau

--- src/ChooseLevelScene.ts
+++ src/ChooseLevelScene.ts
@@ -1,39 +1,51 @@
 import { Button } from './Button'
-import { ChooseLevelScene } from './ChooseLevelScene'
 import { Game } from './Game'
+import { GameScene } from './GameScene'
 import { Point } from './Point'
 import { Mouse } from './Pointer/Mouse'
 import { Pointer } from './Pointer/Pointer'
 import { Scene } from './Scene'
 
-export class HomeScene implements Scene {
+export class ChooseLevelScene implements Scene {
   private game: Game
   private pointer: Pointer
-  private playButton: Button
+  private buttons: Array<Button>
 
   public constructor(game: Game) {
     this.game = game
 
     this.pointer = new Mouse(this.game.getCanvas())
 
-    this.playButton = new Button('Jouer')
-    this.playButton.setPosition(new Point(15, 42))
-    this.playButton.setSize(36, 18)
-    this.playButton.setOnClick((): void => {
-      this.game.changeSceneWithTransition(new ChooseLevelScene(this.game))
-    })
+    const levels = this.game.getStorage().getLevels()
+
+    this.buttons = []
+
+    for (const [ index, level ] of Object.entries(levels)) {
+      const i = parseInt(index, 10)
+
+      const button = new Button(index)
+      button.setPosition(new Point(i * (18 + 4) + 15, 42))
+      button.setSize(18, 18)
+      button.setOnClick((): void => {
+        this.game.changeSceneWithTransition(new GameScene(this.game, i))
+      })
+
+      this.buttons.push(button)
+    }
   }
 
   public update(dt: number): void {
-    this.playButton.update(dt)
-
     const pointerPosition = this.pointer.getPosition()
 
     pointerPosition.x /= this.game.getZoom()
     pointerPosition.y /= this.game.getZoom()
 
-    if (this.pointer.press() && this.playButton.isHover(pointerPosition)) {
-      this.playButton.click()
+    for (const button of this.buttons) {
+      button.update(dt)
+
+      if (this.pointer.press() && button.isHover(pointerPosition)) {
+        button.click()
+      }
     }
   }
 
@@ -50,6 +62,8 @@ export class HomeScene implements Scene {
     ctx.font = '20px Arial'
     ctx.fillText('Bobby', 14, 30)
 
-    this.playButton.render(ctx)
+    for (const button of this.buttons) {
+      button.render(ctx)
+    }
   }
 }
