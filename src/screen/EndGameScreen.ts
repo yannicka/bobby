011a6abf456fd91c125987500244d07126a3d0bc
Yannick Armand <yannick.armand@protonmail.com>
2019-09-28 18:11:07 +0200

    Écran de fin de jeu : bouton de retour à l'écran d'accueil

--- src/screen/EndGameScreen.ts
+++ src/screen/EndGameScreen.ts
@@ -5,8 +5,15 @@ export const EndGameScreen: m.Component = {
     return [
       m('div', { 'class': 'actionbar' }, [
         m('div', { 'class': 'actionbar-gamename' }, 'Bobby'),
+        m('div', { 'class': 'actionbar-menu' }, [
+          m('button', { 'class': 'actionbar-button', 'onclick': goToHome }, 'Aller à l\'écran d\'accueil'),
+        ]),
       ]),
       'Vous avez fini le jeu. Félicitations.',
     ]
   },
+}
+
+function goToHome(e: any): void {
+  m.route.set('/')
 }
