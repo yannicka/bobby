e4bc849302b0dc8dc4a14b9b62247e3c575f2c75
Yannick Armand <yannick.armand@protonmail.com>
2019-11-02 11:43:38 +0100

    Complétion des instructions

--- src/screen/HelpScreen.ts
+++ src/screen/HelpScreen.ts
@@ -9,9 +9,17 @@ export const HelpScreen: m.Component = {
           m(m.route.Link, { 'href': '/', 'class': 'topbar-button' }, 'Retour'),
         ]),
       ]),
-      m('div', { 'class': 'main-wrapper' }, [
+      m('div', { 'class': 'main-wrapper main-wrapper-unaligned' }, [
         m('div', { 'class': 'main-content' }, [
+          m('h2', 'Objectif'),
           m('p', 'Récoltez les pièces dans chaque niveau.'),
+          m('h2', 'Blocs'),
+          m('p', 'Les rochers vous bloque.'),
+          m('p', 'Les tapis roulant vous emmène dans une direction donnée.'),
+          m('p', 'Les boutons ne vous permettent qu\'un nombre restreint de passages.'),
+          m('p', 'Les tourniquets vous bloque certaines directions et tournent dans le sens horaire quand vous en sortez.'),
+          m('p', 'La glace vous fait glisser jusqu\'au prochain bloc solide que vous rencontrez.'),
+          m('p', 'La botte de terrre n\'est traversable que de haut en bas.'),
         ]),
       ]),
     ]
