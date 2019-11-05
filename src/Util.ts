b83df22d9f485dc53a69bb1ab917b38831e8e735
Yannick Armand <yannick.armand@protonmail.com>
2019-11-05 20:39:04 +0100

    Ajout de commentaires dans le fichier Utils.ts

--- src/Util.ts
+++ src/Util.ts
@@ -1,43 +1,70 @@
+/**
+ * Affiche le morceau `index` de l'image `img` sur la zone de dessin `ctx` à la
+ * position `(x, y)` et de taille `(frameWidth, frameHeight)`.
+ *
+ * L'image est découpée en morceaux en fonction de `(frameWidth, frameHeight)`.
+ *
+ * L'indexation se fait ainsi (exemple), en commençant à 0 :
+ *
+ * +---+---+---+
+ * | 0 | 1 | 2 |
+ * +---+---+---+
+ * | 3 | 4 | 5 |
+ * +---+---+---+
+ * | 6 | 7 | 8 |
+ * +---+---+---+
+ */
 export function drawImageByIndex(
   ctx: CanvasRenderingContext2D,
   img: HTMLImageElement,
   x: number,
   y: number,
   index: number,
-  width: number,
-  height: number,
+  frameWidth: number,
+  frameHeight: number,
 ): void {
   // Nombre de tuiles par ligne
-  const nbTilesByLine = Math.ceil(img.width / width)
+  const nbTilesByLine = Math.ceil(img.width / frameWidth)
 
   // Position x depuis laquelle découper le morceau
-  const basex = (index % nbTilesByLine) * width
+  const baseX = (index % nbTilesByLine) * frameWidth
 
   // Position y depuis laquelle découper le morceau
-  const basey = Math.floor(index / nbTilesByLine) * height
+  const baseY = Math.floor(index / nbTilesByLine) * frameHeight
 
   // Afficher le morceau d'image
   ctx.drawImage(
     img,
-    basex,
-    basey,
-    width,
-    height,
+    baseX,
+    baseY,
+    frameWidth,
+    frameHeight,
     x,
     y,
-    width,
-    height,
+    frameWidth,
+    frameHeight,
   )
 }
 
+/**
+ * Délimitation de valeur.
+ *
+ * - Si `num` est inférieur à `min`, retourne `min`  ;
+ * - Si `num` est supérieur à `max`, retourne `max`  ;
+ * - Sinon retourne `num`.
+ */
 export function clamp(num: number, min: number, max: number): number {
   return Math.min(max, Math.max(min, num))
 }
 
 // @todo Retenir le résultat afin d'éviter de le recalculer à chaque appel.
-//
-// @see https://stackoverflow.com/a/4819886
+/**
+ * L'appareil utilisé possède-t-il un écran tactile ?
+ *
+ * @see https://stackoverflow.com/a/4819886
+ */
 export function isTouchDevice(): boolean {
+
   const prefixes = ' -webkit- -moz- -o- -ms- '.split(' ')
 
   const mq = function(mediaQuery: string): boolean {
