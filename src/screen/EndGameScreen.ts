3e015620c81da4c0977100e9cc9474031b2620ba
Yannick Armand <yannick.armand@protonmail.com>
2019-12-04 22:46:56 +0100

    Écran de fin : simplification des textes

--- src/screen/EndGameScreen.ts
+++ src/screen/EndGameScreen.ts
@@ -2,10 +2,6 @@ import m from 'mithril'
 import { Superapp } from '../Superapp'
 import { state } from '../State'
 
-async function delay(ms: number): Promise<unknown> {
-  return new Promise((resolve: () => void) => setTimeout(resolve, ms))
-}
-
 export class EndGameScreen {
   public view(): Array<m.Vnode> {
     return [
@@ -18,7 +14,10 @@ export class EndGameScreen {
 
       m('div', { 'class': 'main-wrapper' }, [
         m('div', { 'class': 'main-content' }, [
-          m('p', { 'id': 'end-game-text', 'class': 'center' }, 'Jeu terminé.'),
+          m('div', { 'id': 'end' }, [
+            m('p', { 'class': 'center' }, 'Félicitations, vous avez fini le jeu. 🎉'),
+            m('p', { 'class': 'center' }, '🥳 Merci d\'avoir joué !'),
+          ]),
         ]),
       ]),
     ]
@@ -30,49 +29,10 @@ export class EndGameScreen {
     }
 
     Superapp.resize()
-
-    const SECOND = 1000
-
-    document.body.classList.add('end-game')
-
-    const endGameText = document.getElementById('end-game-text')
-
-    endGameText.innerText = '🥳 Bravo'
-
-    delay(4 * SECOND).then(async () => {
-      endGameText.innerText = 'Vous avez fini le jeu !'
-
-      return delay(3 * SECOND)
-    }).then(async () => {
-      endGameText.innerText = 'Félicitations. 🎉'
-
-      return delay(3 * SECOND)
-    }).then(async () => {
-      endGameText.innerText = 'Merci d\'avoir joué.'
-
-      return delay(3 * SECOND)
-    }).then(async () => {
-      endGameText.innerText = '🥳 Vous avez terminé le jeu. 🎉'
-
-      return delay(100 * SECOND)
-    }).then(async () => {
-      endGameText.innerText = 'Vous ne pourrez plus connaitre la joie de découvrir le jeu. 😞'
-
-      return delay(50 * SECOND)
-    }).then(async () => {
-      endGameText.innerText = 'Vous pouvez partir.'
-
-      return delay(10 * SECOND)
-    }).then(async () => {
-      endGameText.innerText = 'Vraiment, vous pouvez partir...'
-
-      return delay(10 * SECOND)
-    }).then(() => {
-      endGameText.innerText = 'Vraiment... vous pouvez partir.'
-    })
   }
 
   public onremove(_vnode: m.Vnode): void {
     document.body.classList.remove('end-game')
+
   }
 }
