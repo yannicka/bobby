cf27b33c7aeb9f578c92ed07226069915348a83a
Yannick Armand <yannick.armand@protonmail.com>
2019-12-08 15:11:18 +0100

    Complétion de l'écran d'aide

--- src/screen/HelpScreen.ts
+++ src/screen/HelpScreen.ts
@@ -13,15 +13,19 @@ export class HelpScreen {
 
       m('div', { 'class': 'main-wrapper main-wrapper-unaligned' }, [
         m('div', { 'class': 'main-content' }, [
+          m('h2', 'Touches'),
+          m('p', 'Déplacer le personnage avec les flèches directionnelles 🡨 🡪 🡩 🡫 ou ZQSD, ou bien via la souris ou l\'écran tactile.'),
+
           m('h2', 'Objectif'),
-          m('p', 'Récoltez les pièces dans chaque niveau.'),
+          m('p', 'Réussir tous les niveaux.'),
+          m('p', 'Pour chaque niveau, récupérer toutes les pièces puis atteindre la sortie.'),
 
           m('h2', 'Blocs'),
           m('p', 'Les rochers vous bloque.'),
           m('p', 'Les tapis roulant vous emmène dans une direction donnée.'),
           m('p', 'Les boutons ne vous permettent qu\'un nombre restreint de passages.'),
           m('p', 'Les tourniquets vous bloque certaines directions et tournent dans le sens horaire quand vous en sortez.'),
-          m('p', 'La glace vous fait glisser jusqu\'au prochain bloc solide que vous rencontrez.'),
+          m('p', 'La glace vous fait glisser jusqu\'au prochain vide ou bloc solide que vous rencontrez.'),
         ]),
       ]),
     ]
